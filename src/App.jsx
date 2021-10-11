1db13c4dd1109f058ae05155929fd2e82835408c
eusebiuschipor <eusebiu@eusebiuschipor.com>
2021-10-11 23:19:22 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import productList from "./productList";
+
+const App = () => {
+    return (
+        <>
+            <footer>
+                <p>We bring you <strong>only the best products</strong> that can be randomly generated!</p>
+                <p>Content from <a href="https://marak.github.io/faker.js/">faker.js</a> with images from <a href="https://picsum.photos/">Lorem Picsum</a></p>
+            </footer>
+        </>
+    );
+};
+
+export default App;
